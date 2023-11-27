d43d6d0a619af515e9c4143e9d4efeaf1161a44e
Jesse Abdul <jesse.abdul@noaa.gov>
2023-11-27 11:01:41 -1000

    Updated the data model to integrate the PICDM.DS_PIR_SCOR_V view

--- SQL/queries/grant_data_schema_privs.sql
+++ SQL/queries/grant_data_schema_privs.sql
@@ -12,3 +12,4 @@ grant execute on CEN_UTILS.CEN_UTIL_ARRAY_PKG to CEN_CRUISE with grant option;
 --run this for the PICDM schema
 grant PICDM_INTEG_ROLE TO CEN_CRUISE;
 grant references on PICDM.ODS_INP_DATASET_SCORING to CEN_CRUISE;
+grant select on PICDM.DS_PIR_SCOR_V TO CEN_CRUISE WITH GRANT OPTION;
