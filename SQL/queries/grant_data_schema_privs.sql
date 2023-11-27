83acba25e2d7cd4df8b3702ce6d6378db6c64c67
Jesse Abdul <jesse.abdul@noaa.gov>
2023-11-27 09:28:49 -1000

    Updated grants to CEN_CRUISE schema

--- SQL/queries/grant_data_schema_privs.sql
+++ SQL/queries/grant_data_schema_privs.sql
@@ -11,4 +11,4 @@ grant execute on CEN_UTILS.CEN_UTIL_ARRAY_PKG to CEN_CRUISE with grant option;
 
 --run this for the PICDM schema
 grant PICDM_INTEG_ROLE TO CEN_CRUISE;
-grant references on ODS_INP_DATASET_SCORING to CEN_CRUISE;
+grant references on PICDM.ODS_INP_DATASET_SCORING to CEN_CRUISE;
