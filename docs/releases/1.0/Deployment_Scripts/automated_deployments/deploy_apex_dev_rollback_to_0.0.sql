a7335500bd2312ad201a374c7f801a1fcf2c4197
Jesse Abdul <jesse.abdul@noaa.gov>
2023-12-01 09:31:05 -1000

    bug fix

--- docs/releases/1.0/Deployment_Scripts/automated_deployments/deploy_apex_dev_rollback_to_0.0.sql
+++ docs/releases/1.0/Deployment_Scripts/automated_deployments/deploy_apex_dev_rollback_to_0.0.sql
@@ -1,9 +1,9 @@
 /************************************************************************************
  Filename   : deploy_apex_dev_rollback_to_0.0.sql
  Author     : Jesse Abdul
- Purpose    : Centralized Authorization System apex changes to rollback version 1.1 to 0.0
+ Purpose    : Centralized Authorization System apex changes to rollback version 1.0 to 0.0
 
- Description: The release included reverting changes from the version 0.0 to 1.1 upgrade process
+ Description: The release included reverting changes from the version 0.0 to 1.0 upgrade process
 
 ************************************************************************************/
 SET FEEDBACK ON
@@ -18,13 +18,13 @@ WHENEVER SQLERROR EXIT 1
 WHENEVER OSERROR  EXIT 1
 
 SET DEFINE ON
--- CAS
+-- CCD
 DEFINE apps_credentials=&1
 CONNECT &apps_credentials
 
 
 COL spool_fname NEW_VALUE spoolname NOPRINT
-SELECT 'CAS_DB_deploy_rollback_v1.2_to_v0.0_dev_' || TO_CHAR( SYSDATE, 'yyyymmdd' ) spool_fname FROM DUAL;
+SELECT 'CCD_DB_deploy_rollback_v1.2_to_v0.0_dev_' || TO_CHAR( SYSDATE, 'yyyymmdd' ) spool_fname FROM DUAL;
 SPOOL logs/&spoolname APPEND
 
 
@@ -32,10 +32,10 @@ SET DEFINE OFF
 SHOW USER;
 
 PROMPT rollback the database changes
-@@"../docs/releases/1.1/Deployment_Scripts/rollback/remove_apex_app.sql"
+@@"../docs/releases/1.0/Deployment_Scripts/rollback/remove_apex_app.sql"
 
 PROMPT remove synonyms
-@@"../docs/releases/1.1/Deployment_Scripts/rollback/drop_app_synonyms.sql"
+@@"../docs/releases/1.0/Deployment_Scripts/rollback/drop_app_synonyms.sql"
 
 DISCONNECT;
 
