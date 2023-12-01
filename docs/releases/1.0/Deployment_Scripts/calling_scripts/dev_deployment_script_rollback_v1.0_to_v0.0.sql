08ea304c4512fa1476c015497dee2738560911af
Jesse Abdul <jesse.abdul@noaa.gov>
2023-12-01 09:27:41 -1000

    bug fix

--- docs/releases/1.0/Deployment_Scripts/calling_scripts/dev_deployment_script_rollback_v1.0_to_v0.0.sql
+++ docs/releases/1.0/Deployment_Scripts/calling_scripts/dev_deployment_script_rollback_v1.0_to_v0.0.sql
@@ -4,11 +4,11 @@ cd [working copy root]\SQL
 
 sqlplus /nolog
 
-@"../docs/releases/1.1/Deployment_Scripts/automated_deployments/deploy_dev_rollback_to_0.0.sql"
+@"../docs/releases/1.0/Deployment_Scripts/automated_deployments/deploy_dev_rollback_to_0.0.sql"
 
 sqlplus /nolog
 
-@"../docs/releases/1.1/Deployment_Scripts/automated_deployments/deploy_apex_dev_rollback_to_0.0.sql"
+@"../docs/releases/1.0/Deployment_Scripts/automated_deployments/deploy_apex_dev_rollback_to_0.0.sql"
 
 
 EXIT
